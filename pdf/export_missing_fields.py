6ea4e03d37d642e7f9ddbfa87fe9b3a95ae2cac9
Louis Sallé-Tourne <louis.salletourne@gmail.com>
2024-10-19 14:30:09 -0700

    missing fields

--- pdf/export_missing_fields.py
+++ pdf/export_missing_fields.py
@@ -1,9 +1,11 @@
+import os
+
 from read_pdf.read_pdf import read_pdf_informations
 from read_pdf.missing_field import identify_missing_fields
 from read_pdf.to_json import to_json
 
 pdf_file = "./tests/test_pdf/test/test_1.pdf"
-json_file = "./tests/test_pdf/test/missing_fields.json"
+json_file = "json_file.json"
 
 def export_missing_fields(pdf_file, json_file):
     file, size, metadata = read_pdf_informations(pdf_file)
@@ -11,4 +13,11 @@ def export_missing_fields(pdf_file, json_file):
     to_json(json, json_file)
     with open(json_file, 'r') as file:
         data = json.load(file)
+
+    # Check if the file exists, then delete it
+    if os.path.exists(json_file):
+        os.remove(json_file)
+        print(f"{json_file} has been deleted.")
+    else:
+        print(f"{json_file} does not exist.")
     return [item['field'] for item in data]
