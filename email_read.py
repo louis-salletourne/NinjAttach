53ae2cd1cbd32f248677960132f249f2b54eb268
Louis Sallé-Tourne <louis.salletourne@gmail.com>
2024-10-20 08:15:27 -0700

    credentials

--- email_read.py
+++ email_read.py
@@ -35,14 +35,12 @@ def read_email():
             token_data = json.load(token)
             creds = Credentials.from_authorized_user_info(token_data, SCOPES)
 
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds_json = os.environ.get('GMAIL_CREDENTIALS')
+            creds = json.loads(creds_json)
         # Save the credentials for the next run
         with open('token.json', 'wb') as token:
             token.write(creds.to_json().encode())
